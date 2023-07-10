9ce2120e8581f060062b44effe6a3002786abe0b
Icaro Ferreira <icaro.iky@gmail.com>
2023-07-10 15:12:06 -0300

    Atualização

--- src/server.js
+++ src/server.js
@@ -4,4 +4,8 @@ app.get('/', (req, res) => {
   return res.status(200).json({ message: 'Hello World!' });
 });
 
+app.get('/teste', (req, res) => {
+  return res.status(200).json({ message: "Teste concluído!"});
+})
+
 app.listen(3033, () => console.log('server running on port 3033'));

6755a6210fe4b16f4b24c1102eacb25b41123392
Icaro Ferreira <icaro.iky@gmail.com>
2023-07-10 15:16:48 -0300

    Teste 2

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
 app.get('/', (req, res) => {
-  return res.status(200).json({ message: 'Hello World!' });
+  return res.status(200).json({ message: 'Hello World 2!' });
 });
 
 app.get('/teste', (req, res) => {
